a1adeca925e26642909e77efdaae7492fbc45ecd
Elmlikethetree <maesha.elahi@gmail.com>
2026-07-24 14:29:11 -0400

    Update diagnoses list by removing and adding entries
    Removed 'Appendicitis' and 'Intussusception' from the list of diagnoses and added 'Cholecystitis' and 'Pancreatitis'.

--- Peddle/diagnoses.js
+++ Peddle/diagnoses.js
@@ -127,15 +127,15 @@ const DIAGNOSIS_BANK = [
   "Membranoproliferative glomerulonephritis (MPGN)", 
   "Focal segmental glomerulosclerosis (FSGS)",
   "Transient synovitis", 
-  "Appendicitis", 
   "Malrotation", 
-  "Intussusception", 
   "Ovarian torsion", 
   "Ectopic pregnancy", 
   "Pregnancy", 
   "Testicular torsion", 
   "Small bowel obstruction", 
-  "Large bowel obstruction"
+  "Large bowel obstruction", 
+  "Cholecystitis",
+  "Pancreatitis"
 ];
 
 
